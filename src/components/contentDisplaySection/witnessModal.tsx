d62ae4f2b2b776e580916d65698f228340823cb3
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-12-05 10:53:54 +0800

    feat(witness): user can witness in the post and saved it in the database

---
+++ src/components/contentDisplaySection/witnessModal.tsx
@@ -0,0 +1,36 @@
+import Modal from "../../components/modal"
+import Button from "../../components/button"
+
+interface WitnessModalProps {
+    onClose: () => void;
+    onProceed: () => void
+}
+
+
+export default function WitnessModal({onClose, onProceed } : WitnessModalProps) {
+  return (
+    <Modal>
+        <p className="mb-4">After you click proceed, you cannot undo this action.</p>
+
+        <div
+            className="flex justify-between "
+        >
+            <div className="w-[48%]">
+                <Button
+                    type="button"
+                    buttonText="Cancel"
+                    onClick={onClose}
+                />
+            </div>
+
+            <div className="w-[48%]">
+                <Button
+                    type="button"
+                    buttonText="Proceed"
+                    onClick={onProceed}
+                />
+            </div>
+        </div>
+    </Modal>
+  )
+}
